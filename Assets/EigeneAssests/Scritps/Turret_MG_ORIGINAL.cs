0e01d1caa4a6fe41b28a0154cd59a8bffb9cc004
SheppardX <chelmbold2@googlemail.com>
2014-05-09 15:44:03 +0200

    Turret verbessert 9.05

--- Assets/EigeneAssests/Scritps/Turret_MG_ORIGINAL.cs
+++ Assets/EigeneAssests/Scritps/Turret_MG_ORIGINAL.cs
@@ -1,97 +1,119 @@
 using UnityEngine;
 using System.Collections;
 
-public class Turret_MG_ORIGINAL : MonoBehaviour {
+public class Turret_MG_ORIGINAL : MonoBehaviour
+{
 
-	public static Turret_MG_ORIGINAL instance;
-	public float rotationDamp =2.0f;
-	public GameObject myProjectile;
-    public float reloadTime = 1f;
-	public GameObject myTarget;	
-	public Transform myTargetTran;	
-    public Transform[] muzzlePositions;
-	public float range = 60f;
-	public float distanceToPlayer;
-	public bool hasTarget = false;
-	public Transform turretMG;
-	public ArrayList enemyList = new ArrayList();
-	public int dmgLvl;
-	public int healthLvl;
-	public int rangeLvl;
-	public int[] dmgArray;
-	public int[] healthArray;
-	public int[] rangeArray;
-	public int[] dmgCost;
-	public int[] healthCost;
-	public int[] rangeCost;
-	public GameObject[] enemyObjList = new GameObject[5];
+		public static Turret_MG_ORIGINAL instance;
+		public float rotationDamp = 2.0f;
+		public GameObject myProjectile;
+		public float reloadTime = 1f;
+		public Transform[] muzzlePositions;
+		public float range = 60f;
+		public float distanceToPlayer;
+		public bool hasTarget = false;
+		public Transform turretMG;
+		public ArrayList enemyList = new ArrayList ();
+		public int dmgLvl;
+		public int healthLvl;
+		public int rangeLvl;
+		public int[] dmgArray;
+		public int[] healthArray;
+		public int[] rangeArray;
+		public int[] dmgCost;
+		public int[] healthCost;
+		public int[] rangeCost;
+		public GameObject[] enemyObjList = new GameObject[5];
+		private ParticleEmitter emiter;
+		private Vector3 yOld;
+		private Quaternion rotate;
+		private RaycastHit hit;
+		private Vector3 forward;
+		private Transform objPosition;
+		private float nextFireTime = .5f;
+		private int layerMask = ~((1 << 10) | (1 << 9));
+		private bool placed = false;
+		private bool moved = false;
+		private float startTime = 0.0f;
+		private float duration = 70.0f;
+		private GameObject _currentTarget = null;
 
-	private ParticleEmitter emiter;
-	private Vector3 yOld;
-	private Quaternion rotate;
-	private RaycastHit hit;
-	private Vector3 forward;
-	private Transform objPosition;
-	private float nextFireTime=.5f;
-	private int layerMask = ~((1 << 10)|(1<<9));
-	private bool placed = false;
-	private bool moved = false;
-	private float startTime = 0.0f;
-	private float duration = 70.0f;
-
-	void Awake(){
-		instance = this;
-		dmgLvl = 0;
-		healthLvl = 0;
-		rangeLvl = 0;
-		emiter = GetComponent<ParticleEmitter>();
-		startTime = Time.time;
-		placed = true;
-		objPosition = transform;		
-		yOld = objPosition.position+Turret_Placement.instance.startPos;
-	}
-
-	void Update () {
-		if(placed){			
-			objPosition.position = new Vector3(objPosition.position.x,Mathf.Lerp(objPosition.position.y,yOld.y,(Time.time - startTime) / duration),objPosition.position.z);
-		}
-		enemyObjList = GameObject.FindGameObjectsWithTag("Enemy");
-		for(int i = 0;i<enemyObjList.Length;++i) {
-			forward = turretMG.TransformDirection(Vector3.forward);
-			distanceToPlayer = Vector3.Distance(enemyObjList[i].transform.position, turretMG.position);				
-			if(distanceToPlayer <= range){
-				myTarget = enemyObjList[i];
-				myTargetTran = myTarget.transform;
-				if(myTarget){
-					Vector3 temp = myTargetTran.position;
-					rotate = Quaternion.LookRotation(temp - turretMG.position);	
-					FireProjectile();
+		public Transform CurrentTargetTransform {
+				get {
+						return CurrentTarget.transform;
 				}
-			}
-			else{
-				rotate = Quaternion.identity;	
-			}
-			
-			Debug.DrawRay(turretMG.position,forward*range,Color.red);
-			turretMG.rotation = Quaternion.Lerp(turretMG.rotation, rotate, Time.deltaTime * rotationDamp);
 		}
-	}
+
+		public GameObject CurrentTarget {
+				get {
+						if (_currentTarget != null) 
+								return _currentTarget;
+						else 
+								_currentTarget = searchForNewTarget ();
+
+						return _currentTarget;
+				}
+		}
+
+		private GameObject searchForNewTarget ()
+		{
+			//TODO als globale Singeltonliste 
+			enemyObjList = GameObject.FindGameObjectsWithTag ("Enemy");
 	
+			foreach (GameObject enemy in enemyObjList) {
+				distanceToPlayer = Vector3.Distance (enemy.transform.position, turretMG.position);	
+				if (distanceToPlayer <= range)
+						return enemy;
+			}
+			return null;
+		}
 	
+		void Awake ()
+		{
+				instance = this;
+				dmgLvl = 0;
+				healthLvl = 0;
+				rangeLvl = 0;
+				emiter = GetComponent<ParticleEmitter> ();
+				startTime = Time.time;
+				placed = true;
+				objPosition = transform;		
+				yOld = objPosition.position + Turret_Placement.instance.startPos;
+		}
+
+		void Update ()
+		{
+				if (placed) {			
+						objPosition.position = new Vector3 (objPosition.position.x, Mathf.Lerp (objPosition.position.y, yOld.y, (Time.time - startTime) / duration), objPosition.position.z);
+				}
+
+				GameObject currentTarget = CurrentTarget;
+
+				if (currentTarget == null) {
+						rotate = Quaternion.identity;	
+				} else {
+						rotate = Quaternion.LookRotation (CurrentTargetTransform.position - turretMG.position);	
+						FireProjectile ();
+				}
+				Debug.DrawRay (turretMG.position, forward * range, Color.red);
+				turretMG.rotation = Quaternion.Lerp (turretMG.rotation, rotate, Time.deltaTime * rotationDamp);
+				
+		}
 	
-	void FireProjectile(){
-		myTarget.GetComponent<Creature>().AddjustCurrentHealth(dmgArray[dmgLvl]*Time.deltaTime);
+		void FireProjectile ()
+		{
+				CurrentTarget.GetComponent<Creature> ().AddjustCurrentHealth (dmgArray [dmgLvl] * Time.deltaTime);
 
 		
-	}
+		}
 	
-	
-	IEnumerator wait() {
-		yield return new WaitForSeconds(1);
-	}
+		IEnumerator wait ()
+		{
+				yield return new WaitForSeconds (1);
+		}
 
 
-	/**
+		/**
 	void Update () {
 		if(placed){			
 			objPosition.position = new Vector3(objPosition.position.x,Mathf.Lerp(objPosition.position.y,yOld.y,(Time.time - startTime) / duration),objPosition.position.z);
